9fabc842ecbfc9a5a4570e85393692d7a8fb1a90
Siyuan Zhang <51875984+Cyan010320@users.noreply.github.com>
2021-10-16 16:19:13 +0800

    Initial Commit

---
+++ SmartCanteenVer0.0.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCanteenVer0.0.3
+//
+//  Created by 张思远 on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
